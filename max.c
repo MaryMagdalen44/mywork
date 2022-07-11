2961a4fe28fae496ea90b38ed8b94a5941e25e59
MaryMagdalen44 <104496956+MaryMagdalen44@users.noreply.github.com>
2022-07-11 11:50:36 +0300

    Add files via upload

---
+++ max.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int a, b;
+
+scanf("%d %d", &a, &b);
+ 
+  if (scanf ("%d %d", &a, &b ==2));
+   {if (a>b) {
+    printf("%d",a); } }
+  
+  else { printf("%d",b); } 
+    
+else { printf("n/a"); }
+
+return 0;
+}
+  
